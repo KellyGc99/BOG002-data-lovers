5d96b55160e4cbfabaecaa15e4cc6a218bc948f4
brisney quiñonez <brisneyducuara@gmail.com>
2021-04-22 01:53:27 -0500

    nuevos cambios

--- src/main.js
+++ src/main.js
@@ -1,6 +1,24 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import { example } from './data.js';
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const menuHamburguesa = document.querySelector(".menuhamburguesa");
+menuHamburguesa.addEventListener("click",mostrarMenuNav);
+function mostrarMenuNav () {
+   let nav = document.querySelector(".menu-nav")
+   nav.classList.toggle("mostrar");
+    console.log(nav)
+}
+
+
+
+
+
+
+
+
+
+
+
+import data from './data/lol/lol.js';
+console.log(data);
