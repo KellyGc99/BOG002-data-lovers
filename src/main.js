20d408eb76969f6f06ed46555bd86d338da6b433
brisney quiñonez <brisneyducuara@gmail.com>
2021-04-24 16:02:04 -0500

    cambios con diana

--- src/main.js
+++ src/main.js
@@ -7,12 +7,10 @@ menuHamburguesa.addEventListener("click",mostrarMenuNavegacion);
 function mostrarMenuNavegacion () {
    const navegacion = document.querySelector(".menu-nav")
    navegacion.classList.toggle("mostrar");
-    // console.log(nav)
 }
 
 /* Para mostrar solo la ventana de inicio */
 document.getElementById('todosLosCampeones').style.display='none';
-// document.getElementById('fondoTarjeta').style.display='none';
 
 /* Para mostrar la vista de campeones y ocultar la vista de inicio*/
 let botonCampeones=document.getElementById('botonParaCampeones');
@@ -24,6 +22,22 @@ document.getElementById('contenido1').style.display='none';
 // document.getElementById('fondoPaginaUno').style.display='none';
 }
 
+var elementos = document.querySelectorAll('.botones');
+// Recorremos cada uno de nuestros elementos
+elementos.forEach(function(elemento) {
+  // Obtenemos el texto dentro del div para identificarlo
+  var id = elemento.firstChild;
+  console.log(id)
+  // Agregamos un listener a cada elemento
+  elemento.addEventListener('click', function() {
+    console.log('Elemento ' + id + ' clickeado');
+    document.getElementById("todosLosCampeones").style.display='none';
+    // document.querySelector('contiene').style.display='flex;
+    elemento.classList.toggle('cont');
+});
+  
+});
+
 
 import data from './data/lol/lol.js';
 // console.log(data);
@@ -31,22 +45,8 @@ import data from './data/lol/lol.js';
 const seccionDeCampeones = document.getElementById("todosLosCampeones");
 const campeones = data.data;
 // console.log(campeones)
-let valores = Object.values(campeones); // valores = ["Scott", "Negro", true, 5];
+let valores = Object.values(campeones); 
 for(let i=0; i< valores.length; i++){
     document.getElementById("parrafo").innerHTML +=valores[i].title,valores[i].image.sprite;
-
-  
-
-
-
 }
-// for (const campeon in campeones) {
-//     console.log(campeon)
-    
-//   }
 
-// console.log(Object.keys(campeones))
-
-
-
-    
