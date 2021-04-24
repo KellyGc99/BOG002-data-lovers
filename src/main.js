5e15dae3eaf9c7b623a3c270caedab33ca2265ed
brisney quiñonez <brisneyducuara@gmail.com>
2021-04-23 19:51:19 -0500

    se arreglo la posicion del footer

--- src/main.js
+++ src/main.js
@@ -3,22 +3,50 @@
 // import data from './data/rickandmorty/rickandmorty.js';
 
 const menuHamburguesa = document.querySelector(".menuhamburguesa");
-menuHamburguesa.addEventListener("click",mostrarMenuNav);
-function mostrarMenuNav () {
-   let nav = document.querySelector(".menu-nav")
-   nav.classList.toggle("mostrar");
-    console.log(nav)
+menuHamburguesa.addEventListener("click",mostrarMenuNavegacion);
+function mostrarMenuNavegacion () {
+   const navegacion = document.querySelector(".menu-nav")
+   navegacion.classList.toggle("mostrar");
+    // console.log(nav)
+}
+
+/* Para mostrar solo la ventana de inicio */
+document.getElementById('todosLosCampeones').style.display='none';
+// document.getElementById('fondoTarjeta').style.display='none';
+
+/* Para mostrar la vista de campeones y ocultar la vista de inicio*/
+let botonCampeones=document.getElementById('botonParaCampeones');
+botonCampeones.addEventListener('click', mostrarPaginaCampeones);
+
+function mostrarPaginaCampeones() {
+document.getElementById('todosLosCampeones').style.display='flex';
+document.getElementById('contenido1').style.display='none';
+// document.getElementById('fondoPaginaUno').style.display='none';
 }
 
 
-
-
-
-
-
-
-
-
-
 import data from './data/lol/lol.js';
-console.log(data);
+// console.log(data);
+
+const seccionDeCampeones = document.getElementById("todosLosCampeones");
+const campeones = data.data;
+// console.log(campeones)
+let valores = Object.values(campeones); // valores = ["Scott", "Negro", true, 5];
+for(let i=0; i< valores.length; i++){
+    document.getElementById("parrafo").innerHTML +=valores[i].title,valores[i].image.sprite;
+
+  
+
+
+
+}
+// for (const campeon in campeones) {
+//     console.log(campeon)
+    
+//   }
+
+// console.log(Object.keys(campeones))
+
+
+
+    
