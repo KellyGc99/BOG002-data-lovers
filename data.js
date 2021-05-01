53ad3ceac349927d7cbe58e963929f8fb7111ae3
KellyGc99 <gutierrezkelly88@gmail.com>
2021-05-01 18:42:23 -0500

    Updates

--- data.js
+++ data.js
@@ -1,9 +1,45 @@
 // estas funciones son de ejemplo
+// export const example = () => {
+//   return 'example';
+// };
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
 
-export const example = () => {
-  return 'example';
-};
+// export function sortData(listaDeCampeones) {
+//     let campeonesOrdenados = listaDeCampeones.sort(function (prev, next) {
+//       if (prev.name> next.name) {
+//         return -1;
+//       }
+//       if (prev.name < next.name) {
+//         return 1;
+//       }
+//       return 0;
+//     });
+//     console.log(campeonesOrdenados)
+//     return campeonesOrdenados
+//   }
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+  export function sortData(listaDeCampeones, sortBy) {
+    const ordenar =listaDeCampeones.sort(function(prev, next) {
+      if (prev.name < next.name){
+      return -1;
+      }if (prev.name > next.name){
+        return -1;
+        }
+      return 0
+      })
+      if (sortBy === 'ZA'){
+        return ordenar.reverse()
+      }else{
+    return ordenar
+    }
+  }
+
+ 
+  export function filtradoPorRoles(data,rol){
+    const filtrarData= data.filter(campeon =>    
+      campeon.tags[0] == rol||campeon.tags[1] == rol);
+      return filtrarData;
+  } 
+  
--- main.js
+++ main.js
@@ -1,52 +1,131 @@
 // import { example } from './data.js';
 // import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+// import {pintarCampeones} from './DOMcampeones.js';
 
-const menuHamburguesa = document.querySelector(".menuhamburguesa");
-menuHamburguesa.addEventListener("click",mostrarMenuNavegacion);
-function mostrarMenuNavegacion () {
-   const navegacion = document.querySelector(".menu-nav")
-   navegacion.classList.toggle("mostrar");
-    // console.log(nav)
-}
-
-/* Para mostrar solo la ventana de inicio */
-document.getElementById('todosLosCampeones').style.display='none';
-// document.getElementById('fondoTarjeta').style.display='none';
-
-/* Para mostrar la vista de campeones y ocultar la vista de inicio*/
-let botonCampeones=document.getElementById('botonParaCampeones');
-botonCampeones.addEventListener('click', mostrarPaginaCampeones);
-
-function mostrarPaginaCampeones() {
-document.getElementById('todosLosCampeones').style.display='flex';
-document.getElementById('contenido1').style.display='none';
-// document.getElementById('fondoPaginaUno').style.display='none';
-}
-
-
+import { filtradoPorRoles, sortData } from './data.js';
 import data from './data/lol/lol.js';
-// console.log(data);
 
-const seccionDeCampeones = document.getElementById("todosLosCampeones");
-const campeones = data.data;
-// console.log(campeones)
-let valores = Object.values(campeones); // valores = ["Scott", "Negro", true, 5];
-for(let i=0; i< valores.length; i++){
-    document.getElementById("parrafo").innerHTML +=valores[i].title,valores[i].image.sprite;
-
-  
-
-
-
+/* Para mostrar y quitar la barra de navegación con el menú hamburguesa */
+const menuHamburguesa = document.querySelector(".menuhamburguesa");
+menuHamburguesa.addEventListener("click", mostrarMenuNavegacion);
+function mostrarMenuNavegacion() {
+  const navegacion = document.querySelector(".menu-nav")
+  navegacion.classList.toggle("mostrar");
 }
-// for (const campeon in campeones) {
-//     console.log(campeon)
-    
-//   }
 
-// console.log(Object.keys(campeones))
+/* se oculta la vista de campeones para mostrar solo la ventana de inicio */
+document.getElementById('visualizacionCampeones').style.display = 'none';
+
+/* se muestra la vista de campeones y se oculta la vista de inicio*/
+let botonCampeones = document.getElementById('botonParaCampeones');
+botonCampeones.addEventListener('click', mostrarPaginaCampeones);
+function mostrarPaginaCampeones() {
+  document.getElementById('visualizacionCampeones').style.display = 'flex';
+  document.getElementById('contenido1').style.display = 'none';
+}
+
+/* Para ocultar la vista de campeones y mostrar la vista de inicio haciendo click en la opción home*/
+let botonInicio = document.getElementById("home");
+botonInicio.addEventListener("click", mostrarInicio);
+function mostrarInicio() {
+  document.getElementById('contenido1').style.display = 'flex';
+  document.getElementById('visualizacionCampeones').style.display = 'none';
+}
+
+/* Para ocultar la vista de campeones y mostrar la vista de inicio haciendo click en la imagen logo lol*/
+let logoInicio = document.querySelector(".botonimagen");
+logoInicio.addEventListener("click", mostrarPaginaInicio);
+function mostrarPaginaInicio() {
+  document.getElementById('contenido1').style.display = 'flex';
+  document.getElementById('visualizacionCampeones').style.display = 'none';
+}
+
+/* función para ordenar asc y desc*/
+let opcionesOrdenar = document.getElementById("ordenar");
+opcionesOrdenar.addEventListener("change", () => {
+  let g=opcionesOrdenar.value
+  console.log(g)
+  document.getElementById("todosLosCampeones").innerHTML= "";
+  const resultadoOrdenado = sortData(valores);
+  pintarCampeones(resultadoOrdenado);
+  console.log(resultadoOrdenado);
+});
+
+
+const campeones = data.data;
+let valores = Object.values(campeones);
+
+function pintarCampeones(arrayCampeones) {
+  const seccionCampeones = document.getElementById("todosLosCampeones");
+  for (let i = 0; i < arrayCampeones.length; i++) {
+    // nombreApodo[i]=[valores[i].id,valores[i].title,valores[i].img];
+    // console.log(nombreApodo)
+    const llamandoCampeones = document.createElement("div");
+    llamandoCampeones.className = "estilocampeones";
+    seccionCampeones.appendChild(llamandoCampeones);
+    const nuevoDivImgen = document.createElement("div");
+    nuevoDivImgen.className = "divImgenpersonaje";
+    const nuevaImagen = document.createElement("img");
+    nuevaImagen.className = "imagenPersonaje";
+
+    nuevaImagen.src = arrayCampeones[i].splash;
+    nuevoDivImgen.appendChild(nuevaImagen);
+    llamandoCampeones.appendChild(nuevoDivImgen)
+
+    const nuevoDivContenido = document.createElement("div")
+    nuevoDivContenido.className = "nuevoContenido";
+    const nuevoNombre = document.createElement("h1");
+    nuevoNombre.className = "nombre";
+    const apodo = document.createElement("h2");
+    apodo.className = "nombreapodo";
+
+    nuevoNombre.innerHTML = arrayCampeones[i].name;
+    apodo.innerHTML = arrayCampeones[i].title;
+    nuevoDivContenido.appendChild(nuevoNombre);
+    nuevoDivContenido.appendChild(apodo);
+    llamandoCampeones.appendChild(nuevoDivContenido)
+
+  }
+}
+pintarCampeones(valores);
+
+//Funcion filtrar por roles
+
+const opcionRol = document.getElementById("ordenRol");
+opcionRol.addEventListener("change", () => {
+  let rol=opcionRol.value;
+  document.getElementById("todosLosCampeones").innerHTML= "";
+  pintarCampeones(filtradoPorRoles(valores,rol));
+});
 
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* opcionesOrdenar1.addEventListener("change", function(e){
+  console.log(e)
+});
+
+opcionesOrdenar3.addEventListener("change", elNombreDeMiFunción);
+
+function elNombreDeMiFunción(evento){
+  console.log(evento)
+} */
+
+
